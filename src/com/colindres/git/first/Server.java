f598f6aab38cff3e75e45b36e85fbb6e6cd83939
Nescolindres <nestorcolindres17@gmail.com>
2021-08-04 15:53:46 -0700

    fixed variables

--- src/com/colindres/git/first/Server.java
+++ src/com/colindres/git/first/Server.java
@@ -4,6 +4,13 @@ import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Server {
 
+public class Server extends JFrame{
+
+	private JTextField userText; 			//The box where the user types his message
+	private JTextArea chatWindow; 			//The area where the messages appear during conversation
+	private ObjectOutputStream output;		//The connection from the server to the client. This is the output stream (i.e connection)
+	private ObjectInputStream input;		// The connection from the client to the server.
+	private ServerSocket server;			//Used for port and backlog (i.e queue)
+	private Socket connection; 				//The initial connection to start the server. 
 }
