4f7b4bac9aa5c48b147b98b96796f01d8949f4a2
Nescolindres <nestorcolindres17@gmail.com>
2021-08-04 19:28:57 -0700

    Setup the server start

--- src/com/colindres/git/first/DriverApp.java
+++ src/com/colindres/git/first/DriverApp.java
@@ -1,10 +1,11 @@
 package com.colindres.git.first;
-
+import javax.swing.JFrame;
 public class DriverApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Server server = new Server();
+		server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		server.startRunning();
 	}
 
 }
