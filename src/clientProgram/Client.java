7537e9756467d1a5989d041abdb8dd384f15073b
Nescolindres <nestorcolindres17@gmail.com>
2021-08-05 21:02:02 -0700

    added default constructor

--- src/clientProgram/Client.java
+++ src/clientProgram/Client.java
@@ -1,5 +1,37 @@
 package clientProgram;
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Client {
-
+public class Client extends JFrame {
+	 private JTextField userText;
+	 private JTextArea chatWindow;
+	 private ObjectOutputStream output;
+	 private ObjectInputStream input;
+	 private String message = "";
+	 private String serverIp;
+	 private Socket connection;
+	
+	//constructor
+	public Client (String host) {
+	super("Client user!");
+	userText = new JTextField();
+	userText.setEditable(false);
+	userText.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					sendData(event.getActionCommand());
+					userText.setText("");
+				}
+			}
+		);
+		add(userText, BorderLayout.SOUTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow), BorderLayout.CENTER);
+		setSize(300,150);
+		setVisible(true);
+		
+	}
 }
