6d27b141349dee12603744748a04ef3d0ae95f4f
Nescolindres <nestorcolindres17@gmail.com>
2021-08-05 21:57:51 -0700

    implemented clientapp

--- src/clientProgram/ClientApp.java
+++ src/clientProgram/ClientApp.java
@@ -1,10 +1,13 @@
 package clientProgram;
+import javax.swing.JFrame;
 
 public class ClientApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Client client;
+	client = new Client("127.0.0.1"); //local host is being used as the IP address
+	client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	client.startRunning();
 	}
 
 }
