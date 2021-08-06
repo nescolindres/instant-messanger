6bfcc0cfadb1c30658327ce1fb0e2abab8aaef8d
Nescolindres <nestorcolindres17@gmail.com>
2021-08-05 21:46:50 -0700

    implemented showMessage()

--- src/clientProgram/Client.java
+++ src/clientProgram/Client.java
@@ -91,5 +91,15 @@ public class Client extends JFrame {
 		}
 	}
 	
+	//send messages to server
+	private void sendMessage(String message) {
+		try {
+			output.writeObject("CLIENT -" + message);
+			output.flush();
+			showMessage("\nClient - " + message);
+		}catch(IOException ioException) {
+			chatWindow.append("\n Something messed up sending message");
+		}
+	}
 	
 }
